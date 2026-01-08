e495dd7132f607df12fd8a556e03231e27db047b
JayanTandel <jayantandel13@gmail.com>
2026-01-08 11:17:14 +0530

    js file add by jayan

---
+++ script.js
@@ -0,0 +1,18 @@
+// Change navbar background on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.boxShadow = '0 5px 20px rgba(0,0,0,0.1)';
+    } else {
+        navbar.style.boxShadow = 'none';
+    }
+});
+
+// Simple "Add to Cart" interaction
+const cartButtons = document.querySelectorAll('.add-to-cart');
+cartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const cakeName = button.parentElement.querySelector('h3').innerText;
+        alert(cakeName + ' has been added to your sweet box!');
+    });
+});
